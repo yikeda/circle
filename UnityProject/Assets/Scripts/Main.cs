acf82c1c25ee97b39570025cb9ae671811df3e3a
yu.ikeda <yu.ikeda@gree.net>
2014-08-05 19:56:16 +0900

    サークルの下地を追加

---
+++ UnityProject/Assets/Scripts/Main.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	private List<float> params;
+	
+	void Start () {
+		params = new List<float>();
+	}
+	
+	void Update () {
+	
+	}
+}
